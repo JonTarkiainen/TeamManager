de354a09916abb41254536b08db0c7570dc9aac7
Jon <jon.tarkiainen@kolumbus.fi>
2017-10-22 19:50:55 +0300

    Only one player captain at once

--- TeamManager3/ButtonClickListener.cs
+++ TeamManager3/ButtonClickListener.cs
@@ -54,19 +54,32 @@ namespace TeamManager3
             else if ((string)v.Tag == "Captain")
             {
                 Button button = (Button)v.FindViewById(Resource.Id.buttonCaptain);
+                SetCaptain(player, button);
+            }
+        }
 
-                if (player.isCaptain)
+        private void SetCaptain (Player captainPlayer, Button captainButton)
+        {
+            if (player.isCaptain)
+            {
+                captainButton.SetTextColor(Color.ParseColor("#000000"));
+                captainPlayer.isCaptain = false;
+                Player.UpdatePlayer(captainPlayer);
+            }
+            else
+            {
+                captainButton.SetTextColor(Color.ParseColor("#ff0000"));
+
+                var currentCaptain = Player.GetCaptain();
+
+                if (currentCaptain != null)
                 {
-                    button.SetTextColor(Color.ParseColor("#000000"));
-                    player.isCaptain = false;
-                    Player.UpdatePlayer(player);
+                    currentCaptain.isCaptain = false;
+                    Player.UpdatePlayer(currentCaptain);
                 }
-                else
-                {
-                    button.SetTextColor(Color.ParseColor("#ff0000"));
-                    player.isCaptain = true;
-                    Player.UpdatePlayer(player);
-                }
+
+                captainPlayer.isCaptain = true;
+                Player.UpdatePlayer(captainPlayer);
             }
         }
     }
